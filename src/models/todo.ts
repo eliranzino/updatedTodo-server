36ac3e4353aa192a9419eff8846c35db2f337109
eliranzino <60155151+eliranzino@users.noreply.github.com>
2020-06-11 20:49:45 +0300

    Add files via upload

---
+++ src/models/todo.ts
@@ -0,0 +1,9 @@
+export interface Todo {
+    ID: string;
+    date: Date;
+    Description: string;
+    CreationDate: Date;
+    Complete: boolean;
+    picture: string;
+    UserID: number;
+}
---
+++ src/schemas/todo.ts
@@ -0,0 +1,8 @@
+import joi from "@hapi/joi";
+
+export const todoSchema = joi.object({
+  userId: joi.number().integer().required(),
+  Description: joi.string().min(1).max(500).required(),
+  date: joi.date().required(),
+  picture: joi.string().min(1).max(500),
+});
